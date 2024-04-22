d2931f1f345290195fc85ff2199d10b2b9a84309
luisrobertofelix <105891607+luisrobertofelix@users.noreply.github.com>
2024-04-22 19:35:12 -0300

    com

--- main.js
+++ main.js
@@ -16,6 +16,15 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-31T23:59:59");
+const tempoObjetivo2 = new Date("2024-12-31T23:59:59");
+const tempoObjetivo3 = new Date("2024-12-31T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-31T23:59:59");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+for (let i=0; i < contadores.length; i++) {
+    contadores [i].textContent = calculaTempo (tempos[i]);
+}
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
